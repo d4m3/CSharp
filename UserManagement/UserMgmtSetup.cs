f8ef84a3852ad22f81f922f25953177e20531813
DForbes <damianoforbes@gmail.com>
2018-01-28 17:44:44 -0500

    Initial Commit

---
+++ UserManagement/UserMgmtSetup.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.Globalization;
+using System.Data;
+
+namespace UserManagement
+{
+    public class UserMgmtSetup
+    {
+        string strCon = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\Forbes\source\repos\AccessDB";// & Application.StartupPath + "\AdminDatabase.accdb";
+
+        OleDbConnection connection;
+        OleDbCommand command;
+        DataSet datast;
+        OleDbDataAdapter adapter;
+        BindingSource bindingsrc;
+        OleDbDataReader reader;
+
+        string sql;
+
+        DateTime dt = DateTime.Now;
+        DateTimeFormatInfo dtfInfo = DateTimeFormatInfo.InvariantInfo;
+        string myDay = "dd";
+        string myMonth = "MMM";
+        string myYear = "yyyy";
+
+        string dtID;
+        int accType;
+        bool IsFind = false;
+
+    }
+}
