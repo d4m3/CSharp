0ebcebbb732d8bafb3d5e114f9a024020b3b8abc
DForbes <damianoforbes@gmail.com>
2018-01-30 18:14:45 -0500

    Initial Commit

---
+++ UIUX/RocketAnimation/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace RocketAnimation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.linerocket = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.rocket = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.rocket)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ball
+            // 
+            this.ball.Location = new System.Drawing.Point(391, 262);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(6, 6);
+            this.ball.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ball.TabIndex = 0;
+            this.ball.TabStop = false;
+            // 
+            // linerocket
+            // 
+            this.linerocket.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(172)))), ((int)(((byte)(221)))));
+            this.linerocket.Location = new System.Drawing.Point(340, 320);
+            this.linerocket.Name = "linerocket";
+            this.linerocket.Size = new System.Drawing.Size(104, 4);
+            this.linerocket.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(41)))), ((int)(((byte)(46)))));
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(341, 326);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(104, 89);
+            this.panel1.TabIndex = 1;
+            // 
+            // rocket
+            // 
+            this.rocket.BackColor = System.Drawing.Color.Transparent;
+            this.rocket.Location = new System.Drawing.Point(362, 323);
+            this.rocket.Name = "rocket";
+            this.rocket.Size = new System.Drawing.Size(64, 64);
+            this.rocket.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.rocket.TabIndex = 2;
+            this.rocket.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 10F);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(4, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Please wait..";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(41)))), ((int)(((byte)(46)))));
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.rocket);
+            this.Controls.Add(this.linerocket);
+            this.Controls.Add(this.ball);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.rocket)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.Panel linerocket;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox rocket;
+    }
+}
+
---
+++ UIUX/RocketAnimation/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RocketAnimation
+{
+    public partial class Form1 : Form
+    {
+        Timer timeback = new Timer();
+        Timer timerrocket = new Timer();
+        Timer timerball = new Timer();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            timeback.Tick += new EventHandler(Resource);
+            timerrocket.Tick += new EventHandler(Ball);
+            timerball.Tick += new EventHandler(RocketUp);
+
+            RocketFunction();
+        }
+
+        //
+        void RocketFunction()
+        {
+            linerocket.Height = 1; linerocket.Width = 0; linerocket.Left = 341;
+            ball.Top = 262; ball.Visible = false; ball.BringToFront();
+            rocket.Height = 64; rocket.Top = 320; rocket.Visible = false;
+
+            // Image Locations
+            ball.ImageLocation = "ball2.png";
+            rocket.ImageLocation = "whiterocket.png";
+            ImageAnimator.UpdateFrames();
+
+
+            // Time Intervals
+            timeback.Interval = 30;
+            timeback.Interval = 40;
+            timerrocket.Interval = 10;
+
+            timeback.Start();
+        }
+
+        // Initial
+        int wd = 0; int down = 1, lt = 5;
+        void Resource(object sender, EventArgs e)
+        {
+            linerocket.Width += wd;
+            if (linerocket.Width < 1)
+            {
+                wd = 8;
+            }
+            if (linerocket.Width > 104)
+            {
+                timeback.Stop();
+                timerrocket.Start();
+            }
+            
+            //anim.ShowSync(label1);
+        }
+
+        // Ball        
+        void Ball(object sender, EventArgs e)
+        {
+            ball.Top += down;
+            if (ball.Top < 265)
+            {
+                ball.Visible = true;
+                down = 1;
+            }
+            if (ball.Top > 305)
+            {
+                timerball.Interval = 10; // increase speed
+                rocket.ImageLocation = "rocket.png";
+
+                ball.Visible = false;
+                down = 5;
+                rocket.Top -= down;
+                if (rocket.Top < -60)
+                {
+                    linerocket.Width -= lt;
+                    linerocket.Left += lt;
+                    if (linerocket.Left > 470)
+                    {
+                        down = 1;
+                        timerball.Stop();
+                        RocketFunction();
+                    }
+                }
+            }
+        }
+
+        // Rocket
+        int rockettop = -1;
+        void RocketUp(object sender, EventArgs e)
+        {
+            rocket.Top += rockettop;
+            if (rocket.Top > 318)
+            {
+                rocket.Visible = true;
+                rockettop = -5;
+            }
+            if (rocket.Top < 250)
+            {
+                rockettop = -1;
+                timerrocket.Stop();
+                timerball.Start();
+            }
+        }
+    }
+}
