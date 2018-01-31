a4d45d0560d564e2277e4bc52d1888d462ec6a20
DForbes <damianoforbes@gmail.com>
2018-01-30 21:42:43 -0500

    Initial Commit

---
+++ UIUX/LoadingProgressBar/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace LoadingProgressBar
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelLoading = new System.Windows.Forms.Label();
+            this.labelBy = new System.Windows.Forms.Label();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.labelPercent = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelLoading
+            // 
+            this.labelLoading.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.labelLoading.ForeColor = System.Drawing.Color.White;
+            this.labelLoading.Location = new System.Drawing.Point(219, 197);
+            this.labelLoading.Name = "labelLoading";
+            this.labelLoading.Size = new System.Drawing.Size(346, 31);
+            this.labelLoading.TabIndex = 0;
+            this.labelLoading.Text = "LOADING";
+            this.labelLoading.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelBy
+            // 
+            this.labelBy.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.labelBy.ForeColor = System.Drawing.Color.White;
+            this.labelBy.Location = new System.Drawing.Point(219, 239);
+            this.labelBy.Name = "labelBy";
+            this.labelBy.Size = new System.Drawing.Size(346, 31);
+            this.labelBy.TabIndex = 0;
+            this.labelBy.Text = "By Damian Forbes";
+            this.labelBy.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // progressBar
+            // 
+            this.progressBar.BackColor = System.Drawing.Color.Gainsboro;
+            this.progressBar.Location = new System.Drawing.Point(186, 273);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(412, 5);
+            this.progressBar.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.AutoEllipsis = true;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Bold);
+            this.button1.ForeColor = System.Drawing.Color.LightSeaGreen;
+            this.button1.Location = new System.Drawing.Point(310, 302);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(164, 48);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Next";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // labelPercent
+            // 
+            this.labelPercent.AutoSize = true;
+            this.labelPercent.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.labelPercent.ForeColor = System.Drawing.Color.White;
+            this.labelPercent.Location = new System.Drawing.Point(183, 281);
+            this.labelPercent.Name = "labelPercent";
+            this.labelPercent.Size = new System.Drawing.Size(15, 17);
+            this.labelPercent.TabIndex = 0;
+            this.labelPercent.Text = "0";
+            this.labelPercent.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(116)))), ((int)(((byte)(62)))));
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.labelPercent);
+            this.Controls.Add(this.labelBy);
+            this.Controls.Add(this.labelLoading);
+            this.Name = "Form";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Loading Screen";
+            this.Load += new System.EventHandler(this.FormLoad);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelLoading;
+        private System.Windows.Forms.Label labelBy;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label labelPercent;
+    }
+}
+
---
+++ UIUX/LoadingProgressBar/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoadingProgressBar
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        Timer timer1 = new Timer();
+        Timer timer2 = new Timer();
+        public Form()
+        {
+            InitializeComponent();
+            LoadingFunction();
+        }
+
+        // Load
+        void LoadingFunction()
+        {
+            this.labelLoading.Top = 229;
+            this.labelBy.Top = 220;
+            this.labelPercent.Left = 184;
+            this.labelBy.Visible = false;
+            this.labelLoading.BringToFront();
+            this.progressBar.Value = 0;
+        }
+
+        int plus = 1;
+        int lblplus = 4;
+        void Move(object sender, EventArgs e)
+        {
+            if (labelPercent.Left < 587 && progressBar.Value < 100) 
+            {
+                progressBar.Value += plus;
+                labelPercent.Left += lblplus;
+                labelPercent.Text = progressBar.Value.ToString() + "%";
+            }
+            if (labelPercent.Text == "100%")
+            {
+                timer1.Stop();
+                timer2.Start();
+            }
+        }
+
+        private void FormLoad(object sender, EventArgs e)
+        {
+            timer1.Tick += new EventHandler(Move);
+            timer2.Tick += new EventHandler(Move);
+
+            timer1.Interval = 20;
+            timer2.Interval = 10;
+
+            timer1.Start();
+        }
+    }
+}
