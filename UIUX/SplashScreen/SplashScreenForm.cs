cb030d8768c290f95ff709f18ff58931b8d22548
DForbes <damianoforbes@gmail.com>
2018-01-31 22:01:20 -0500

    Initial Commit

---
+++ UIUX/SplashScreen/SplashScreenForm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SplashScreen
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        public Form()
+        {
+            InitializeComponent();
+        }
+
+        private void Form_Load(object sender, EventArgs e)
+        {
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.Width = this.BackgroundImage.Width;
+            this.Height = this.BackgroundImage.Height;
+
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            timer.Start();
+            this.Close();
+        }
+    }
+}
