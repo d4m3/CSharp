f6d374145537787db097e42b2a84b96de4eff561
DForbes <damianoforbes@gmail.com>
2018-02-25 12:33:39 -0500

    GUI Dashboard Initial Commit

---
+++ _PROJECTS/Crystal Reports/CrysDataTable/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CrysDataTable
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Load table with test data
+        private void crvDataTable_Load(object sender, EventArgs e)
+        {
+            // Declare datatable
+            DataTable Product = new DataTable();
+            Product.Columns.Add("SlNo",typeof(int));
+            Product.Columns.Add("Name", typeof(int));
+            Product.Columns.Add("Price", typeof(int));
+            Product.Columns.Add("Count", typeof(int));
+
+            // Insert test rows
+            Product.Rows.Add(1,"Pen", 20, 250);
+            Product.Rows.Add(2, "Pencil", 10, 150);
+            Product.Rows.Add(3, "Notebook", 60, 550);
+            Product.Rows.Add(4, "Bag", 700, 750);
+            Product.Rows.Add(5, "Umbrella", 50, 500);
+        }
+    }
+}
---
+++ _PROJECTS/GUI Dashboard/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace GUI_Dashboard
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.applicationsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tACITToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.anakamToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pointClickCareToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.clearTranToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.labelPrinterQueueToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timelessBreastmilkToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kPassLawsonLookupToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.infocliqueKaleidascopeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.comdocToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.orderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.inventoryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.reportsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.serverToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.metricsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.feedbackToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contactToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.applicationsToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Padding = new System.Windows.Forms.Padding(6, 7, 0, 5);
+            this.menuStrip.Size = new System.Drawing.Size(850, 31);
+            this.menuStrip.TabIndex = 0;
+            this.menuStrip.Text = "menuStrip1";
+            // 
+            // applicationsToolStripMenuItem
+            // 
+            this.applicationsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tACITToolStripMenuItem,
+            this.reportsToolStripMenuItem,
+            this.serverToolStripMenuItem,
+            this.metricsToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.applicationsToolStripMenuItem.Name = "applicationsToolStripMenuItem";
+            this.applicationsToolStripMenuItem.Size = new System.Drawing.Size(85, 19);
+            this.applicationsToolStripMenuItem.Text = "Applications";
+            // 
+            // tACITToolStripMenuItem
+            // 
+            this.tACITToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.anakamToolStripMenuItem,
+            this.pointClickCareToolStripMenuItem,
+            this.clearTranToolStripMenuItem,
+            this.labelPrinterQueueToolStripMenuItem,
+            this.timelessBreastmilkToolStripMenuItem,
+            this.kPassLawsonLookupToolStripMenuItem,
+            this.infocliqueKaleidascopeToolStripMenuItem,
+            this.comdocToolStripMenuItem});
+            this.tACITToolStripMenuItem.Name = "tACITToolStripMenuItem";
+            this.tACITToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.tACITToolStripMenuItem.Text = "TAC - IT";
+            // 
+            // anakamToolStripMenuItem
+            // 
+            this.anakamToolStripMenuItem.Name = "anakamToolStripMenuItem";
+            this.anakamToolStripMenuItem.Size = new System.Drawing.Size(207, 22);
+            this.anakamToolStripMenuItem.Text = "Anakam";
+            // 
+            // pointClickCareToolStripMenuItem
+            // 
+            this.pointClickCareToolStripMenuItem.Name = "pointClickCareToolStripMenuItem";
+            this.pointClickCareToolStripMenuItem.Size = new System.Drawing.Size(207, 22);
+            this.pointClickCareToolStripMenuItem.Text = "Point Click Care";
+            // 
+            // contextMenuStrip
+            // 
+            this.contextMenuStrip.Name = "contextMenuStrip";
+            this.contextMenuStrip.Size = new System.Drawing.Size(61, 4);
+            // 
+            // clearTranToolStripMenuItem
+            // 
+            this.clearTranToolStripMenuItem.Name = "clearTranToolStripMenuItem";
+            this.clearTranToolStripMenuItem.Size = new System.Drawing.Size(207, 22);
+            this.clearTranToolStripMenuItem.Text = "ClearTran";
+            // 
+            // labelPrinterQueueToolStripMenuItem
+            // 
+            this.labelPrinterQueueToolStripMenuItem.Name = "labelPrinterQueueToolStripMenuItem";
+            this.labelPrinterQueueToolStripMenuItem.Size = new System.Drawing.Size(207, 22);
+            this.labelPrinterQueueToolStripMenuItem.Text = "Label Printer Queue";
+            // 
+            // timelessBreastmilkToolStripMenuItem
+            // 
+            this.timelessBreastmilkToolStripMenuItem.Name = "timelessBreastmilkToolStripMenuItem";
+            this.timelessBreastmilkToolStripMenuItem.Size = new System.Drawing.Size(207, 22);
+            this.timelessBreastmilkToolStripMenuItem.Text = "Timeless Breastmilk";
+            // 
+            // kPassLawsonLookupToolStripMenuItem
+            // 
+            this.kPassLawsonLookupToolStripMenuItem.Name = "kPassLawsonLookupToolStripMenuItem";
+            this.kPassLawsonLookupToolStripMenuItem.Size = new System.Drawing.Size(207, 22);
+            this.kPassLawsonLookupToolStripMenuItem.Text = "KPass Lawson Lookup";
+            // 
+            // infocliqueKaleidascopeToolStripMenuItem
+            // 
+            this.infocliqueKaleidascopeToolStripMenuItem.Name = "infocliqueKaleidascopeToolStripMenuItem";
+            this.infocliqueKaleidascopeToolStripMenuItem.Size = new System.Drawing.Size(207, 22);
+            this.infocliqueKaleidascopeToolStripMenuItem.Text = "Infoclique / Kaleidascope";
+            // 
+            // comdocToolStripMenuItem
+            // 
+            this.comdocToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.orderToolStripMenuItem,
+            this.inventoryToolStripMenuItem});
+            this.comdocToolStripMenuItem.Name = "comdocToolStripMenuItem";
+            this.comdocToolStripMenuItem.Size = new System.Drawing.Size(207, 22);
+            this.comdocToolStripMenuItem.Text = "Comdoc";
+            // 
+            // orderToolStripMenuItem
+            // 
+            this.orderToolStripMenuItem.Name = "orderToolStripMenuItem";
+            this.orderToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.orderToolStripMenuItem.Text = "Order";
+            // 
+            // inventoryToolStripMenuItem
+            // 
+            this.inventoryToolStripMenuItem.Name = "inventoryToolStripMenuItem";
+            this.inventoryToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.inventoryToolStripMenuItem.Text = "Inventory";
+            // 
+            // reportsToolStripMenuItem
+            // 
+            this.reportsToolStripMenuItem.Name = "reportsToolStripMenuItem";
+            this.reportsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.reportsToolStripMenuItem.Text = "Reports";
+            // 
+            // serverToolStripMenuItem
+            // 
+            this.serverToolStripMenuItem.Name = "serverToolStripMenuItem";
+            this.serverToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.serverToolStripMenuItem.Text = "Server";
+            // 
+            // metricsToolStripMenuItem
+            // 
+            this.metricsToolStripMenuItem.Name = "metricsToolStripMenuItem";
+            this.metricsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.metricsToolStripMenuItem.Text = "Metrics";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem,
+            this.feedbackToolStripMenuItem,
+            this.contactToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 19);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // feedbackToolStripMenuItem
+            // 
+            this.feedbackToolStripMenuItem.Name = "feedbackToolStripMenuItem";
+            this.feedbackToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.feedbackToolStripMenuItem.Text = "Feedback";
+            // 
+            // contactToolStripMenuItem
+            // 
+            this.contactToolStripMenuItem.Name = "contactToolStripMenuItem";
+            this.contactToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.contactToolStripMenuItem.Text = "Contact";
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(850, 500);
+            this.Controls.Add(this.menuStrip);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "Form";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem applicationsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tACITToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem anakamToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pointClickCareToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem clearTranToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem labelPrinterQueueToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem timelessBreastmilkToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem kPassLawsonLookupToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem infocliqueKaleidascopeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem comdocToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem orderToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem inventoryToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem reportsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem serverToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem metricsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem feedbackToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem contactToolStripMenuItem;
+    }
+}
+
