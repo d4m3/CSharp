569890416dbf79233cfa7ea098bcf956c90bd921
DForbes <damianoforbes@gmail.com>
2018-06-20 21:28:51 -0400

    Slider Panel

---
+++ SlidingPanel/SlidingPanel/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace SlidingPanel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.panelHeader = new System.Windows.Forms.Panel();
+            this.panelMenu = new System.Windows.Forms.Panel();
+            this.panelSlide = new System.Windows.Forms.Panel();
+            this.btnHideShow = new System.Windows.Forms.Button();
+            this.btnHosts = new System.Windows.Forms.Button();
+            this.btnReports = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnExit = new System.Windows.Forms.Button();
+            this.panelSliderBar = new System.Windows.Forms.Panel();
+            this.panelContainer = new System.Windows.Forms.Panel();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.panelHeader.SuspendLayout();
+            this.panelMenu.SuspendLayout();
+            this.panelContainer.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelHeader
+            // 
+            this.panelHeader.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.panelHeader.Controls.Add(this.btnExit);
+            this.panelHeader.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelHeader.Location = new System.Drawing.Point(275, 0);
+            this.panelHeader.Name = "panelHeader";
+            this.panelHeader.Size = new System.Drawing.Size(978, 35);
+            this.panelHeader.TabIndex = 0;
+            this.panelHeader.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panelHeader_MouseDown);
+            // 
+            // panelMenu
+            // 
+            this.panelMenu.BackColor = System.Drawing.Color.Lavender;
+            this.panelMenu.Controls.Add(this.panelSliderBar);
+            this.panelMenu.Controls.Add(this.button7);
+            this.panelMenu.Controls.Add(this.button6);
+            this.panelMenu.Controls.Add(this.button5);
+            this.panelMenu.Controls.Add(this.button4);
+            this.panelMenu.Controls.Add(this.btnReports);
+            this.panelMenu.Controls.Add(this.btnHosts);
+            this.panelMenu.Controls.Add(this.btnHideShow);
+            this.panelMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelMenu.Name = "panelMenu";
+            this.panelMenu.Size = new System.Drawing.Size(50, 500);
+            this.panelMenu.TabIndex = 1;
+            // 
+            // panelSlide
+            // 
+            this.panelSlide.BackColor = System.Drawing.Color.OrangeRed;
+            this.panelSlide.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelSlide.Location = new System.Drawing.Point(50, 0);
+            this.panelSlide.Name = "panelSlide";
+            this.panelSlide.Size = new System.Drawing.Size(225, 500);
+            this.panelSlide.TabIndex = 2;
+            // 
+            // btnHideShow
+            // 
+            this.btnHideShow.FlatAppearance.BorderSize = 0;
+            this.btnHideShow.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnHideShow.Image = ((System.Drawing.Image)(resources.GetObject("btnHideShow.Image")));
+            this.btnHideShow.Location = new System.Drawing.Point(10, 3);
+            this.btnHideShow.Name = "btnHideShow";
+            this.btnHideShow.Size = new System.Drawing.Size(35, 35);
+            this.btnHideShow.TabIndex = 1;
+            this.btnHideShow.UseVisualStyleBackColor = true;
+            this.btnHideShow.Click += new System.EventHandler(this.btnHideShow_Click);
+            // 
+            // btnHosts
+            // 
+            this.btnHosts.FlatAppearance.BorderSize = 0;
+            this.btnHosts.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnHosts.Image = ((System.Drawing.Image)(resources.GetObject("btnHosts.Image")));
+            this.btnHosts.Location = new System.Drawing.Point(10, 54);
+            this.btnHosts.Name = "btnHosts";
+            this.btnHosts.Size = new System.Drawing.Size(35, 35);
+            this.btnHosts.TabIndex = 1;
+            this.btnHosts.UseVisualStyleBackColor = true;
+            this.btnHosts.Click += new System.EventHandler(this.btnHosts_Click);
+            // 
+            // btnReports
+            // 
+            this.btnReports.FlatAppearance.BorderSize = 0;
+            this.btnReports.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReports.Image = ((System.Drawing.Image)(resources.GetObject("btnReports.Image")));
+            this.btnReports.Location = new System.Drawing.Point(10, 119);
+            this.btnReports.Name = "btnReports";
+            this.btnReports.Size = new System.Drawing.Size(35, 35);
+            this.btnReports.TabIndex = 1;
+            this.btnReports.UseVisualStyleBackColor = true;
+            this.btnReports.Click += new System.EventHandler(this.btnReports_Click);
+            // 
+            // button4
+            // 
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Image = ((System.Drawing.Image)(resources.GetObject("button4.Image")));
+            this.button4.Location = new System.Drawing.Point(10, 185);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(35, 35);
+            this.button4.TabIndex = 1;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.FlatAppearance.BorderSize = 0;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Image = ((System.Drawing.Image)(resources.GetObject("button5.Image")));
+            this.button5.Location = new System.Drawing.Point(10, 239);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(35, 35);
+            this.button5.TabIndex = 1;
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.FlatAppearance.BorderSize = 0;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.Image = ((System.Drawing.Image)(resources.GetObject("button6.Image")));
+            this.button6.Location = new System.Drawing.Point(10, 307);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(35, 35);
+            this.button6.TabIndex = 1;
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.FlatAppearance.BorderSize = 0;
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button7.Image = ((System.Drawing.Image)(resources.GetObject("button7.Image")));
+            this.button7.Location = new System.Drawing.Point(10, 417);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(35, 35);
+            this.button7.TabIndex = 1;
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 30;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.btnExit.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.btnExit.FlatAppearance.BorderSize = 0;
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExit.Image = ((System.Drawing.Image)(resources.GetObject("btnExit.Image")));
+            this.btnExit.Location = new System.Drawing.Point(943, 0);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(35, 35);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // panelSliderBar
+            // 
+            this.panelSliderBar.BackColor = System.Drawing.Color.OrangeRed;
+            this.panelSliderBar.Location = new System.Drawing.Point(2, 3);
+            this.panelSliderBar.Name = "panelSliderBar";
+            this.panelSliderBar.Size = new System.Drawing.Size(5, 35);
+            this.panelSliderBar.TabIndex = 2;
+            // 
+            // panelContainer
+            // 
+            this.panelContainer.Controls.Add(this.chart1);
+            this.panelContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelContainer.Location = new System.Drawing.Point(275, 35);
+            this.panelContainer.Name = "panelContainer";
+            this.panelContainer.Size = new System.Drawing.Size(978, 465);
+            this.panelContainer.TabIndex = 3;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Location = new System.Drawing.Point(25, 35);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.EarthTones;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(796, 137);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(31)))), ((int)(((byte)(31)))));
+            this.ClientSize = new System.Drawing.Size(1253, 500);
+            this.Controls.Add(this.panelContainer);
+            this.Controls.Add(this.panelHeader);
+            this.Controls.Add(this.panelSlide);
+            this.Controls.Add(this.panelMenu);
+            this.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.panelHeader.ResumeLayout(false);
+            this.panelMenu.ResumeLayout(false);
+            this.panelContainer.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelHeader;
+        private System.Windows.Forms.Panel panelMenu;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button btnReports;
+        private System.Windows.Forms.Button btnHosts;
+        private System.Windows.Forms.Button btnHideShow;
+        private System.Windows.Forms.Panel panelSlide;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Panel panelSliderBar;
+        private System.Windows.Forms.Panel panelContainer;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ SlidingPanel/SlidingPanel/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SlidingPanel
+{
+    public partial class Form1 : Form
+    {
+        int panelWidth;
+        bool Hidden;
+
+        public Form1()
+        {
+            InitializeComponent();
+            //btnHhost1.Visible = false;
+            //btnHost2.Visible = false;
+            //panelHostButtons.Visible = false;            
+            panelSliderBar.Height = btnHideShow.Height;            
+            panelWidth = panelSlide.Width;
+            Hidden = false;
+        }
+
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(IntPtr hwnd, int wmsg, int wparam, int lparam);
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnHideShow_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            panelSliderBar.Height = btnHideShow.Height;
+            panelSliderBar.Top = btnHideShow.Top;
+        }
+
+        private void btnHosts_Click(object sender, EventArgs e)
+        {
+            panelSliderBar.Height = btnHosts.Height;
+            panelSliderBar.Top = btnHosts.Top;
+            //if (panelSliderBar.Top == btnHosts.Top)
+            //{
+            //    panelHostButtons.Visible = true;
+            //    btnHhost1.Visible = true;
+            //    btnHost2.Visible = true;
+            //}
+            //else
+            //{
+            //    panelHostButtons.Visible = false;
+            //    btnHhost1.Visible = false;
+            //    btnHost2.Visible = false;
+            //}
+        }
+
+        private void btnReports_Click(object sender, EventArgs e)
+        {
+            panelSliderBar.Height = btnReports.Height;
+            panelSliderBar.Top = btnReports.Top;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (Hidden)
+            {
+                panelSlide.Width = panelSlide.Width + 10;
+                if (panelSlide.Width >= panelWidth)
+                {
+                    timer1.Stop();
+                    Hidden = false;
+                    this.Refresh();
+                }
+            }
+            else
+            {
+                panelSlide.Width = panelSlide.Width - 10;
+                if (panelSlide.Width <= 0)
+                {
+                    timer1.Stop();
+                    Hidden = true;
+                    this.Refresh();
+                }
+            }
+        }
+
+        private void panelHeader_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112,0xf012,0);
+        }
+    }
+}
